96d297ce53d30021b6d997a14f9c46e26de05a40
Gaurav Meena <gauravmeena0708@gmail.com>
2025-01-19 00:58:59 +0530

    working

--- build/lib/whatsapp_analyzer/__init__.py
+++
@@ -1,5 +0,0 @@
-# whatsapp_analyzer/__init__.py
-# You can add any package-level initialization here if needed.
-# For example, downloading NLTK data:
-import nltk
-nltk.download('stopwords', quiet=True)
